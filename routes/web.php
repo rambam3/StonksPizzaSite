298a70ad80798c6eb5538643db3c0adc19d255a5
Corwin <134613242+CorwinSUMMA@users.noreply.github.com>
2025-01-14 13:00:00 +0100

    menu, home

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PizzaController;
-use App\Http\Controllers\BestellingController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,15 +12,24 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/menu', function () {
+    return view('menu');
+})->name('menu');
+
+route::get('/klant/home', function () {
+    return view('klant.home');
+})->name('klant.home');
+
+route::get('/klant/menu', function () {
+    return view('klant.menu');
+})->name('klant.menu');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-
-Route::resource('home', PizzaController::class);
-Route::resource('menu', PizzaController::class);
 
 
 
