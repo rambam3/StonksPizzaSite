a03430c86c36995fc460110de9b5bd9ec5b4346d
rambam3 <ps252728@edu.summacollege.nl>
2025-01-16 11:02:45 +0100

    Add bestelMethode view with afhalen and bezorgen sections

---
+++ resources/views/klant/bestelMethode.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <div>
+        <h1 class="text-4xl font-bold ml-8">Bestel Methode</h1>
+        <div>
+            <div>
+                <h2 class="text-2xl font-bold ml-8">Afhalen</h2>
+
+            </div>
+            <div>
+                <h2 class="text-2xl font-bold ml-8">Bezorgen</h2>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
