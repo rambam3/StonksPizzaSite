4d25c813fc09492b6fa8ead54de5ed635eee5c77
Corwin <134613242+CorwinSUMMA@users.noreply.github.com>
2025-01-20 10:56:57 +0100

    test

--- app/Http/Controllers/BestellingController.php
+++ app/Http/Controllers/BestellingController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class BestellingController extends Controller
 {
+    public function showBestelMethode()
+    {
+        return view('klant.bestelMethode');
+    }
     /**
      * Display a listing of the resource.
      */
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PizzaController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\BestellingController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -31,6 +32,7 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/menu', [PizzaController::class, 'index'])->name('menu');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::post('/submit-contact', [ContactController::class, 'submit']);
+Route::get('/bestel-methode', [BestellingController::class, 'showBestelMethode'])->name('bestelMethode');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
